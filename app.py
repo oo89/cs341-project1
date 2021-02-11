041210e2860659497c82744dd4cd5e496ac89037
Oscar <oo89@njit.edu>
2021-02-11 05:07:34 +0000

    DFA added need to be check all test cases

--- app.py
+++ app.py
@@ -16,18 +16,121 @@ def checkString(stringData):
     
     for ch in stringData :
         print("Current character: " + ch)
-        
+        #First state that is q0 check if it is Γ
         if state == 0 : 
-            
             if ch.isalpha() and ch.islower():
                 state = 1
                 printState(state)
-
-def printState(state): 
-    
+            else: # check this 
+                state = -1 
+                printState(state)
+                
+        #q1 check if it is Γ*, ∆, Φ
+        elif state == 1:
+            if ch.isalpha() and ch.islower():
+                state = 1 
+                printState(state)
+            elif ch == '.':
+                state = 3
+                printState(state)
+            elif ch == '@':
+                state = 2 
+                printState(state)
+            else:
+                state = -1 
+                printState(state)
+                
+        #q3 will check for Γ and if it is other will go to a trap state. 
+        elif state == 3:
+            if ch.isalpha() and ch.islower():
+                state = 1 
+                printState(state)
+            # Trap State 
+            else: # check this 
+                state = -1 
+                printState(state)
+                
+        #q2 check Γ or other 
+        elif state == 2:
+            if ch.isalpha() and ch.islower():
+                state = 5 
+                printState(state)
+            else: # check this 
+                state = -1  
+                printState(state)
+                
+        #q5 heck Γ* or ∆        
+        elif state == 5:
+            if ch.isalpha() and ch.islower():
+                state = 5
+                printState(state)
+            elif ch == '.':
+                state = 6
+                printState(state)
+            else: # check this here I could uses trap state q4
+                state = -1 
+                printState(state)
+        #q6 check n or Γ-n          
+        elif state == 6: 
+            if ch == 'n':
+                state = 7 
+                printState(state)
+            elif ch.isalpha() and ch.islower() and ch != 'n':
+                state = 5 
+                printState(state)
+            else: # check this 
+                state = -1 
+                printState(state)
+        #q7 check e, ∆ or Γ-e  
+        elif state == 7: 
+            if ch == 'e': 
+                state = 8
+                printState(state)
+            elif ch.isalpha() and ch.islower() and ch != 'e':
+                state = 5 
+                printState(state)
+            elif ch == '.': 
+                state = 6 
+                printState(state)
+            else:
+                state = -1 
+                printState(state)
+        #q8 check t, Γ-e, ∆ 
+        elif state == 8: 
+            if ch == 't':
+                state = 9 
+                printState(state)
+            elif ch == '.': 
+                state = 6
+                printState(state)
+            elif ch.isalpha() and ch.islower() and ch != 't':
+                state = 5 
+                printState(state)
+            else:
+                state = -1 
+                printState(state)
+        #q9 check Γ, ∆, trap with Φ. This is a final state 
+        elif state == 9:
+            if ch == '.':
+                state = 6 
+                printState(state)
+            elif ch.isalpha() and ch.islower():
+                state = 5 
+                printState(state)
+            else: # check this. here if the enter @ will brake and also other different tan the other two.
+                state = 10 
+                printState(state)
+                
+    # final state after the for 
+    if state == 9: 
+        print("String accepted.")
+    else:
+        print("String rejected.")
+        
+def printState(state):
     if state == -1: 
         print("String rejected.")
-        os._exit   
+        exit()   
     else:
         print("State: q" + str(state))
             
